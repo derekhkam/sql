ade5f24561153663e2726db928edcbc6a7bc8859
Derek <derekhkam@yahoo.ca>
2024-05-26 00:20:09 -0400

    I edited the homewrk_4.sql file

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -16,9 +16,10 @@ HINT: keep the syntax the same, but edited the correct components with the strin
 The `||` values concatenate the columns into strings. 
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
-
-
-
+SELECT 
+product_name || ', ' || coalesce(product_size, '') || ' (' || coalesce(product_qty_type,'unit') || ')'
+	AS product_name_size_qty_type
+FROM product
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -29,12 +30,25 @@ You can either display all rows in the customer_purchases table, with the counte
 each new market date for each customer, or select only the unique market dates per customer 
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
-
+SELECT *, dense_rank() 
+	OVER(PARTITION BY customer_id ORDER BY market_date ASC) 
+		AS customer_purchase_date_number
+FROM customer_purchases
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
-
-
+SELECT *
+FROM (
+	SELECT *, dense_rank() 
+		OVER(PARTITION BY customer_id ORDER BY market_date DESC) 
+			AS customer_purchase_date_number
+	FROM customer_purchases
+	) x
+WHERE x.customer_purchase_date_number = 1
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+SELECT *, count()
+	OVER(PARTITION BY customer_id, product_id) 
+		AS customer_product_times
+FROM customer_purchases
